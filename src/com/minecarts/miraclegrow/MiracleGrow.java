3695719c9f9e145e767e1132b700194ba204ccb8
Kevin Ingersoll <kingersoll@gmail.com>
2011-12-06 03:54:58 -0800

    Initial restoration code, but about to redo table structure

--- src/com/minecarts/miraclegrow/MiracleGrow.java
+++ src/com/minecarts/miraclegrow/MiracleGrow.java
@@ -17,6 +17,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.World;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
 import org.bukkit.event.Event.Type;
 import org.bukkit.event.Event.Priority;
@@ -38,8 +40,12 @@ public class MiracleGrow extends org.bukkit.plugin.java.JavaPlugin {
     protected DBQuery dbq;
     protected Provider provider;
     
+    protected int flushInterval;
+    protected int restoreInterval;
+    
     protected HashMap<World, HashSet<BlockStateRestore>> queue = new HashMap<World, HashSet<BlockStateRestore>>();
-    protected ArrayList<World> processing = new ArrayList<World>();
+    protected ArrayList<World> flushing = new ArrayList<World>();
+    protected ArrayList<World> restoring = new ArrayList<World>();
     
     
     public void onEnable() {
@@ -60,37 +66,66 @@ public class MiracleGrow extends org.bukkit.plugin.java.JavaPlugin {
             }
         }
         
-        // TODO: check and create tables from config IF NOT EXIST
+        
         
         /*
-            CREATE TABLE IF NOT EXISTS `MiracleGrow_table_name` (
+            CREATE TABLE IF NOT EXISTS `MiracleGrow_world_blocks` (
               `x` smallint(6) NOT NULL DEFAULT '0',
               `y` tinyint(4) NOT NULL DEFAULT '0',
               `z` smallint(6) NOT NULL DEFAULT '0',
               `type` smallint(6) DEFAULT NULL,
               `data` tinyint(4) DEFAULT NULL,
-              `when` timestamp DEFAULT NULL,
+              PRIMARY KEY (`x`,`y`,`z`)
+            ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+            
+            CREATE TABLE IF NOT EXISTS `MiracleGrow_world_jobs` (
+              `x` smallint(6) NOT NULL DEFAULT '0',
+              `y` tinyint(4) NOT NULL DEFAULT '0',
+              `z` smallint(6) NOT NULL DEFAULT '0',
+              `when` timestamp NULL DEFAULT NULL,
+              `job` int(11) DEFAULT NULL,
               PRIMARY KEY (`x`,`y`,`z`),
-              KEY `when` (`when`)
+              INDEX `when` (`when`),
+              INDEX `job` (`job`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
          */
         
         
-        int interval = Math.max(20, 20 * config.getInt("flushInterval"));
-        debug("Flushing block restore queue at a {0} tick interval", interval);
         
-        getServer().getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+        getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
             public void run() {
-                processQueue();
+                debug("Flushing block restore queue");
+                flushQueue();
+                
+                // reschedule task with potentially changed flush interval
+                getServer().getScheduler().scheduleSyncDelayedTask(MiracleGrow.this, this, flushInterval);
             }
-        }, interval, interval);
+        }, flushInterval);
+        
+        
+        getServer().getScheduler().scheduleSyncDelayedTask(this, new Runnable() {
+            public void run() {
+                if(!restoring.isEmpty() || !flushing.isEmpty()) {
+                    debug("Flush or restore in progress, rescheduling restore task for 5 seconds from now");
+                    getServer().getScheduler().scheduleSyncDelayedTask(MiracleGrow.this, this, 20 * 5);
+                    return;
+                }
+                
+                debug("Restoring blocks");
+                restoreBlocks();
+                
+                // reschedule task with potentially changed restore interval
+                getServer().getScheduler().scheduleSyncDelayedTask(MiracleGrow.this, this, restoreInterval);
+            }
+        }, restoreInterval);
+        
         
 
         log("Version {0} enabled.", getDescription().getVersion());
     }
     
     public void onDisable() {
-        processQueue(false);
+        flushQueue(false);
     }
     
     
@@ -103,6 +138,12 @@ public class MiracleGrow extends org.bukkit.plugin.java.JavaPlugin {
         debug = config.getBoolean("debug");
         worlds = config.getConfigurationSection("worlds");
         provider = dbq.getProvider(config.getString("DBQuery.provider"));
+        
+        flushInterval = Math.max(20, 20 * config.getInt("flushInterval"));
+        debug("Flushing block restore queue to database at a {0} tick interval", flushInterval);
+        
+        restoreInterval = Math.max(20, 20 * config.getInt("restoreInterval"));
+        debug("Restoring blocks from database at a {0} tick interval", restoreInterval);
     }
     
     
@@ -159,10 +200,10 @@ public class MiracleGrow extends org.bukkit.plugin.java.JavaPlugin {
     }
     
     
-    private void processQueue() {
-        processQueue(true);
+    private void flushQueue() {
+        flushQueue(true);
     }
-    private void processQueue(boolean async) {
+    private void flushQueue(boolean async) {
         if(worlds == null) {
             debug("No table names found in worlds section of configuration file");
             return;
@@ -171,11 +212,16 @@ public class MiracleGrow extends org.bukkit.plugin.java.JavaPlugin {
         for(Entry<World, HashSet<BlockStateRestore>> entry : queue.entrySet()) {
             
             final World world = entry.getKey();
-            String table = worlds.getString(world.getName(), null);
+            final String table = worlds.getString(world.getName(), null);
             HashSet<BlockStateRestore> set = entry.getValue();
             
-            if(processing.contains(world)) {
-                debug("Waiting to process world {0}...", world.getName());
+            if(restoring.contains(world)) {
+                debug("Restore in progress for world {0}...", world.getName());
+                continue;
+            }
+            
+            if(flushing.contains(world)) {
+                debug("Flush already in progress for world {0}...", world.getName());
                 continue;
             }
             
@@ -190,7 +236,8 @@ public class MiracleGrow extends org.bukkit.plugin.java.JavaPlugin {
                 continue;
             }
             
-            processing.add(world);
+            debug("Flushing block restore queue for world {0}", world.getName());
+            flushing.add(world);
             
             
             StringBuilder sql = new StringBuilder("INSERT INTO `").append(table).append("` (`x`, `y`, `z`, `type`, `data`, `when`) VALUES ");
@@ -216,7 +263,7 @@ public class MiracleGrow extends org.bukkit.plugin.java.JavaPlugin {
                 @Override
                 public void onAffected(Integer affected) {
                     debug("{0} rows affected", affected);
-                    processing.remove(world);
+                    flushing.remove(world);
                 }
 
                 @Override
@@ -233,7 +280,7 @@ public class MiracleGrow extends org.bukkit.plugin.java.JavaPlugin {
                         else {
                             log("FAILED! SQLException on Query: {0}", query);
                             e.printStackTrace();
-                            processing.remove(world);
+                            flushing.remove(world);
                         }
                     }
                     catch(com.minecarts.dbquery.NoConnectionException e) {
@@ -245,18 +292,189 @@ public class MiracleGrow extends org.bukkit.plugin.java.JavaPlugin {
                         else {
                             log("FAILED! NoConnectionException on Query: {0}", query);
                             e.printStackTrace();
-                            processing.remove(world);
+                            flushing.remove(world);
                         }
                     }
                     catch(Exception e) {
                         log("FAILED! Exception on Query: {0}", query);
                         e.printStackTrace();
-                        processing.remove(world);
+                        flushing.remove(world);
                     }
                 }
             }.affected(params.toArray());
             
         }
+    }
+    
+    
+    private void restoreBlocks() {
+        if(!restoring.isEmpty() || !flushing.isEmpty()) {
+            debug("Flush or restore in progress...");
+            return;
+        }
+        
+        if(worlds == null) {
+            debug("No table names found in worlds section of configuration file");
+            return;
+        }
+        
+        for(final World world : getServer().getWorlds()) {
+            
+            final String table = worlds.getString(world.getName(), null);
+            if(table == null) {
+                debug("No table name found for worlds.{0}, clearing world's block queue", world.getName());
+                continue;
+            }
+            
+            debug("Restoring blocks for world {0}", world);
+            restoring.add(world);
+            
+            
+            final int restoreId = (int) (System.currentTimeMillis() / 1000L);
+            StringBuilder sql = new StringBuilder("UPDATE `").append(table).append("` SET `restore_id`=? WHERE `when` <= NOW() ORDER BY `restore_id`, `when` LIMIT 1000");
+            
+            new Query(sql.toString()) {
+                @Override
+                public void onAffected(Integer affected) {
+                    if(affected > 0) {
+                        debug("Updated {0} rows for restore job #{1,number,#}", affected, restoreId);
+                    }
+                    else {
+                        debug("No rows updated for restore job #{0,number,#}", restoreId);
+                        restoring.remove(world);
+                        return;
+                    }
+                    
+                    StringBuilder sql = new StringBuilder("SELECT `x`, `y`, `z`, `type`, `data` FROM `").append(table).append("` WHERE `restore_id`=? ORDER BY `y`, `x`, `z`");
+                    new Query(sql.toString()) {
+                        private int tries = 0;
+
+                        @Override
+                        public void onFetch(ArrayList<HashMap> rows) {
+                            if(rows.size() > 0) {
+                                debug("Got {0} rows for restore job #{1,number,#}", rows.size(), restoreId);
+                            }
+                            else {
+                                debug("No rows found for restore job #{0,number,#}", restoreId);
+                                restoring.remove(world);
+                                return;
+                            }
+                            
+                            
+                            // check chunks for player entities
+                            // TODO: skip jobs with unloaded chunks?
+                            for(HashMap<String, Integer> row : rows) {
+                                int x = row.get("x").intValue() >> 4;
+                                int z = row.get("z").intValue() >> 4;
+                                
+                                if(!world.isChunkLoaded(x, z)) continue;
+                                
+                                for(Entity entity : world.getChunkAt(x, z).getEntities()) {
+                                    if(entity instanceof Player) {
+                                        debug("Player entity found in chunk [{0} {1}], skipping restore job #{2,number,#}", x, z, restoreId);
+                                        restoring.remove(world);
+                                        return;
+                                    }
+                                }
+                            }
+                            
+                            
+                            int successes = 0;
+                            int failures = 0;
+                            
+                            for(HashMap<String, Integer> row : rows) {
+                                int x = row.get("x").intValue();
+                                int y = row.get("y").intValue();
+                                int z = row.get("z").intValue();
+                                int type = row.get("type").intValue();
+                                byte data = row.get("data").byteValue();
+                                
+                                Block block = world.getBlockAt(x, y, z);
+                                if(type == block.getTypeId() && data == block.getData()) {
+                                    // block is correct, no restore necessary
+                                    continue;
+                                }
+                                
+                                if(block.setTypeIdAndData(type, data, false)) {
+                                    successes++;
+                                }
+                                else {
+                                    // TODO: retry or re-add to block restore queue?
+                                    debug("Failed to restore block at [{0} {1} {2}] to {3}:{4}", x, y, z, type, data);
+                                    failures++;
+                                }
+                            }
+                            
+                            debug("{0}/{1} blocks didn''t need restoring", rows.size() - successes - failures, rows.size());
+                            debug("{0}/{1} blocks successfully restored", successes, rows.size());
+                            if(failures > 0) debug("{0}/{1} blocks failed", failures, rows.size());
+                            
+                            
+                            
+                            StringBuilder sql = new StringBuilder("DELETE FROM `").append(table).append("` WHERE `restore_id`=?");
+                            
+                            new Query(sql.toString()) {
+                                @Override
+                                public void onAffected(Integer affected) {
+                                    debug("Deleted {0} rows from table {1} for block restore job #{2,number,#}", affected, table, restoreId);
+                                    restoring.remove(world);
+                                }
+                                @Override
+                                public void onException(Exception e, FinalQuery query) {
+                                    e.printStackTrace();
+                                    restoring.remove(world);
+                                }
+                            }.affected(restoreId);
+                        }
+
+                        @Override
+                        public void onException(Exception x, FinalQuery query) {
+                            try {
+                                throw x;
+                            }
+                            catch(java.sql.SQLException e) {
+                                if(++tries < 5) {
+                                    log("SQLException on Query, retrying...");
+                                    e.printStackTrace();
+                                    query.run();
+                                }
+                                else {
+                                    log("FAILED! SQLException on Query: {0}", query);
+                                    e.printStackTrace();
+                                    restoring.remove(world);
+                                }
+                            }
+                            catch(com.minecarts.dbquery.NoConnectionException e) {
+                                if(++tries < 5) {
+                                    log("NoConnectionException on Query, retrying...");
+                                    e.printStackTrace();
+                                    query.run();
+                                }
+                                else {
+                                    log("FAILED! NoConnectionException on Query: {0}", query);
+                                    e.printStackTrace();
+                                    restoring.remove(world);
+                                }
+                            }
+                            catch(Exception e) {
+                                log("FAILED! Exception on Query: {0}", query);
+                                e.printStackTrace();
+                                restoring.remove(world);
+                            }
+                        }
+                    }.fetch(restoreId);
+                }
+                
+                @Override
+                public void onException(Exception e, FinalQuery query) {
+                    e.printStackTrace();
+                    restoring.remove(world);
+                }
+            }.affected(restoreId);
+            
+        }
+            
+            
     }
     
     
